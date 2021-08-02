8b401cb4090ac480bbf3f9f5280654f7952ec107
sejin.kim1904@gmail.com <sejin.kim1904@gmail.com>
2021-08-02 11:52:36 -0600

    Updates exports for healthcheck

--- source/controllers/health.ts
+++ source/controllers/health.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 const serverHealthCheck = (_req: Request, res: Response, _next: NextFunction) => {
-    return res.status(200).json({
+    res.status(200).json({
         message: 'pong'
     });
 };
