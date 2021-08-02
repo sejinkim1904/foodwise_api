8fce9c6a11563547bf4e4455037223ac7a2aa2d6
sejin.kim1904@gmail.com <sejin.kim1904@gmail.com>
2021-08-02 11:51:15 -0600

    Update test configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,1 +1,6 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+const preset = 'ts-jest';
 const testEnvironment = 'node';
+const transform = { '^.+\\.ts?$': 'ts-jest' };
+const moduleFileExtensions = ['ts', 'js', 'json', 'node'];
+const testPathIgnorePatterns = ['/node_modules/', '/<rootDir>/source/config'];
