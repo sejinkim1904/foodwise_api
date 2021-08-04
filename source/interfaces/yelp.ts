3dc8aff99e1074129dc3b16eb4f1f8b0d0fc598e
sejin.kim1904@gmail.com <sejin.kim1904@gmail.com>
2021-08-03 21:12:01 -0600

    Create yelp interface

---
+++ source/interfaces/yelp.ts
@@ -0,0 +1,18 @@
+export interface YelpBusiness {
+    name: string;
+    url: string;
+    rating: number;
+    distance: number;
+    location: YelpBusinessLocation;
+    phone: string;
+}
+
+export interface YelpBusinessLocation {
+    address1: string;
+    address2?: string;
+    address3?: string;
+    city: string;
+    zip_code: string;
+    country: string;
+    state: string;
+}
